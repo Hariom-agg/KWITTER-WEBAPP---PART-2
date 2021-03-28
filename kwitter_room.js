946eeed342ca86c203f86564a8116eb9a1bd6326
Hariom-agg <74823540+Hariom-agg@users.noreply.github.com>
2021-03-28 14:39:21 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyA1Gbf-FXXhJPq43Yd2ZfB_rLaK-cI4M9E",
+    authDomain: "kwiter-project.firebaseapp.com",
+    databaseURL: "https://kwiter-project-default-rtdb.firebaseio.com",
+    projectId: "kwiter-project",
+    storageBucket: "kwiter-project.appspot.com",
+    messagingSenderId: "879424491102",
+    appId: "1:879424491102:web:418dd1f751c732e1b86ecf"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
